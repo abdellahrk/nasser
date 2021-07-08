365857475b84cdcca19bf06064ea1e23bb03aa06
giantthinker <ramadanabdel24@gmail.com>
2021-07-08 12:35:20 +0200

    Configure the test and its dependencies

---
+++ tests/Entity/UserTest.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace App\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{ 
+    public function testCreateUser()
+    {
+        $user = new User();
+
+        
+    }
+}
