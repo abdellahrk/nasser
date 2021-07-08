a6b83e1ffbfe5c346f2aa6fae3718f78851e5b11
giantthinker <ramadanabdel24@gmail.com>
2021-07-08 17:26:53 +0200

    Work on frontend

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/dashlite.css';
+import './styles/theme.css';
+
+// JS
+import './js/bundle.js';
+import './js/gd-default';
+import './js/scripts.js';
+
+// start the Stimulus application
+import './bootstrap';
