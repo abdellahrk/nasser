45552523337a8bd3c6bbc97f9d4dfee1c7c0b45f
Abdellah <ramadanabdel24@gmail.com>
2024-07-16 18:46:20 +0100

    update code for new requirements

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,22 +12,19 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class AdminController extends AbstractController
 {
     #[Route('/configure-admin', name: 'admin')]
-    public function index(UserPasswordHasherInterface $passwordHasher): Response
+    public function index(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em): Response
     {
         $admin = new User();
         
-        $admin->setEmail('admin@eastwest-financial.com');
-        $admin->setUsername('abdel-admin');
+        $admin->setEmail('admin@nasser.build');
+        $admin->setUsername('bonjour');
         $admin->setRoles(["ROLE_ADMIN", "ROLE_USER"]);
         $admin->setPassword(
             $passwordHasher->hashPassword($admin, 'bonjour1')
         );
 
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $entityManager->persist($admin);
-
-        $entityManager->flush();
+        $em->persist($admin);
+        $em->flush();
         
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
